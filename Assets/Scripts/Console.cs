380d131c3bd52f10cd98ec995ffea084328aeb98
Rio <rio.imam@gmail.com>
2022-09-17 16:42:06 +0700

    Separate Command

---
+++ Assets/Scripts/Console.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Console : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+    public string[] commandsPerLine;
+    public string[] runningCommand;
+
+    public string commandClass;
+    public string commandMethod;
+    public string commandParams;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Separate()
+    {
+        string legalChars = "1234567890";
+
+        commandsPerLine = inputField.text.Split(char.Parse("\n"));
+        runningCommand = commandsPerLine[0].Split(char.Parse("."));
+        
+        commandClass = runningCommand[0];
+        commandMethod = runningCommand[1];
+
+        foreach (var letter in commandMethod)    // Extract number
+        {
+            if(legalChars.Contains(letter))
+            {
+                commandParams += letter;
+            }
+        }
+    }
+}
