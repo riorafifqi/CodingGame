138e1e4af2f5f4791d8f5858b992c5006e18b859
Aldo Zulhaq <44134685+aldozulhaq@users.noreply.github.com>
2022-10-22 00:54:17 +0700

    FinishTile

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,15 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
     GameManager gameManager;
     public GameObject winPanel;
+    Material[] lampMat;
 
     private void Awake()
     {
-        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        //gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        lampMat = GameObject.Find("BezierCurve.007").GetComponent<Renderer>().materials;
+        Debug.Log(lampMat[4].name);
+        lampMat[4].SetColor("_EmissionColor", Color.blue);
+        lampMat[4].SetColor("_Color", Color.blue);
     }
 
     private void Start()
@@ -19,9 +22,9 @@ public class FinishLine : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.tag == "Player")
+        if (other.transform.tag == "Player")
         {
-            if(gameManager.isVirusGone)
+            if (gameManager.isVirusGone)
             {
                 Debug.Log("You Win!");
                 winPanel.SetActive(true);
@@ -31,5 +34,10 @@ public class FinishLine : MonoBehaviour
                 Debug.Log("There's still virus left");
             }
         }
+    }
+
+    public void activateFinish()
+    {
+        lampMat[1].SetColor("_EmissionColor", Color.green);
     }
 }
