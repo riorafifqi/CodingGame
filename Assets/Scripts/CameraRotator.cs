41d3d97da0f6bfbdc31cc06fa97be9e19d19cf1b
Aldo Zulhaq <imahumanty@gmail.com>
2022-11-18 14:43:36 +0700

    Models and stuff

---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
